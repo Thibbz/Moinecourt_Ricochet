8e5dee4c44d900c105136d210e97003de9b15db3
Thibbz <65372633+Thibbz@users.noreply.github.com>
2022-06-02 17:43:25 +0200

    Update Time.java

--- Ricochet/src/main/java/isep/jfx/Time.java
+++ Ricochet/src/main/java/isep/jfx/Time.java
@@ -1,5 +1,12 @@
 package isep.jfx;
 
+/*
+Classe permettant le fonctionnement du Timer
+
+Attribut : second => le nombre de seconde écoulé
+
+Fonction : getter, constructeur, oneSecondPassed => incrémente de 1 seconde
+ */
 public class Time {
 
     private int second;
